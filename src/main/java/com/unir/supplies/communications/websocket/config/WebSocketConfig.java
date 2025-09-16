002f0876f9b26b1f7304e9b32fccdcbeba570655
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2025-09-17 00:18:21 +0200

    Minimal WebSocket config

--- src/main/java/com/unir/supplies/communications/websocket/config/WebSocketConfig.java
+++ src/main/java/com/unir/supplies/communications/websocket/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.unir.supplies.communications.websocket;
+package com.unir.supplies.communications.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -19,6 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws-api/v1/communications")
-			.setAllowedOrigins("*");
+			.setAllowedOrigins("*"); //Test to remove this (using GW)
 	}
 }
